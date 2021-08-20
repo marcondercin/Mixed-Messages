8a0cc3b5df3522f01faff09d9321dca9b6d460d0
marcondercin <marcondercin@gmail.com>
2021-08-20 15:03:01 -0700

    Come up with some message parts

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Script for the Mixed Messages project
 
-// Just a little test.
-// And another one.
-// And one more.
-// What about now.
+const part1 = [
+    'If you want,',
+    'Something you should think about is',
+    'Really all there is to life is chocolate,',
+    'All signs point to the road less travelled,',
+    'If you ever wanted to step up,'
+];
+
+const part2 = [
+    'try starting a book',
+    'start exercising more',
+    'look at your phone less',
+    'go see a pal',
+    'drink more water'
+];
+
+const part3 = [
+    'on a boat.',
+    'in your home.',
+    'on top of a car.',
+    'inside the smallest box you can fit in.',
+    'next to a dog.'
+];
+
